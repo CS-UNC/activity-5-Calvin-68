43c9c345ff4297d16547fc0ac53be69b561e8f42
Calvin-68 <nest0611@bears.unco.edu>
2025-11-06 19:31:58 +0000

    has no e

--- activity5.py
+++ activity5.py
@@ -8,15 +8,14 @@ def more_than_20(words_file):
             result.append(word)
     return result
             
-print (more_than_20)
+
     
 def has_no_e(word):
-    for i in word:
-        if "e" not in i:
-            return False
-        else: 
-            return True
+    item = str(word)
+    return 'e' in item.lower()
         
+
+
 def uses_only(word, letters):
     for letter in word:
         if letter not in letters:
@@ -24,8 +23,5 @@ def uses_only(word, letters):
         else: 
             return True
     
-
-print(more_than_20(words_file))
-
 
 
