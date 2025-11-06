bca5120886d577171a4e1771b8409e934d2d9452
Calvin-68 <nest0611@bears.unco.edu>
2025-11-06 19:42:21 +0000

    more than 20

--- activity5.py
+++ activity5.py
@@ -1,14 +1,15 @@
 words_file =open('CROSSWD.txt','r')
 
-def more_than_20(words_file):
+def more_than_20(file):
     result = []
+    words_file =open('CROSSWD.txt','r')
     for item in words_file:
         word = item.strip('\n')
         if len(word) >= 20:
             result.append(word)
     return result
             
-
+print(more_than_20('CROSSWD.txt'))
     
 def has_no_e(word):
     item = str(word)
